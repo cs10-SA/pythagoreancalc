6b09eb9bfb4819264f40a3c7447f986a35c4c5d5
cs10-SA <145050215+cs10-SA@users.noreply.github.com>
2023-10-18 11:00:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// CS10 grade calculator
+// Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+// Store order code in a function
+function btnClicked() {
+  // INPUT
+  let sideA = +document.getElementById("a-in").value;
+  let sideB = +document.getElementById("b-in").value;
+
+  // PROCESS
+  let total = Math.sqrt(sideA ** 2 + sideB ** 2);
+
+  // OUTPUT
+  document.getElementById("output").innerHTML = total;
+}
